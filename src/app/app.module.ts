b0f140bade4ed92cd60a3f0c50636416d3ec7321
frebrezo <frebrezo@gmail.com>
2019-08-02 13:52:17 -0700

    Added new component, new-cmp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCmpComponent // Added by "ng generate component new-cmp".
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // for bootstrap the AppComponent the main app component is given.
 })
 export class AppModule { }
